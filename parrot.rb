97adf1894a3440b92449ad631bc5ccb05e7d9938
Dominique Demetz <ddemetz123@icloud.com>
2017-06-20 19:08:01 +0000

    Waffles?

--- parrot.rb
+++ parrot.rb
@@ -1,17 +1,29 @@
 # Happy Parrot - This parrot is so happy. It accepts a 'thing' as its argument and then returns a string where it says how happy it is about the thing!
 def happy_parrot(thing)
-  "I am so happy about #{thing}!"
+p "I am so happy about #{thing}!"
 end
+happy_parrot("coding")
 
 # Boring Parrot - Write a method for a boring parrot that just returns whatever string you give him as an argument.
-
-
+def boring_parrot(string)
+p "I dont care about #{string}!"
+end
+boring_parrot("coding")
 
 # Math Parrot - Create a method that accepts two numbers as arguments and adds them together! 
+def math_parrot(number1, number2)
+p number1 + number2
+end
+math_parrot(2, 3)
 
 
 
 # Friendly Parrot - This parrot greets people by returning their name and age (don't forget to pass that information in as arguments).
+p "What's your name!"
+name=gets.chomp
+p "How old are you?"
+age=gets.chomp
+p "Nice to meet you, #{name}. You are #{age} years old."
 
 
 
